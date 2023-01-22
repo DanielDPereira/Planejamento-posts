35b59b13b60e78f59946bbb0f037d4e628da4ed0
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-01-22 11:23:11 -0300

    Create planejamento.py

---
+++ planejamento.py
@@ -0,0 +1,38 @@
+import calendar
+
+'''
+yy = 2023
+mm = 2
+
+print(calendar.month(yy, mm))
+
+'''
+
+
+def Ordena_dic(d): #<--Função para ordenar o dicionário original
+    return dict(sorted(d.items(), key=lambda t:t[1]))
+
+#Input dos valores que serão inseridos no dicionário original
+V_Alerta1 = int(input("V Alerta "))
+V_Confirma1 = int(input("V Confirma "))
+V_Explica1 = int(input("V Explica "))
+V_Indica1 = int(input("V Indica "))
+Verus_ODS1 = int(input("V ODS "))
+
+#Criando dicionário original
+dict_posts1 = {
+    "V_Alerta": V_Alerta1,
+    "V_Confirma": V_Confirma1,
+    "V_Explica": V_Explica1,
+    "V_Indica": V_Indica1,
+    }
+
+print(dict_posts1)
+
+#Ordenando o dicionário original e criando em um segund dicionário, já ordenado
+dict_posts2 = Ordena_dic(dict_posts1)
+
+print(dict_posts2)
+
+#Só para não fechar no cmd
+input()
