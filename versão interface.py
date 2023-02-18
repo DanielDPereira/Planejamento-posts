fef668e4d2821cdc3fb5128e89b08b5cf2d61fa6
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-02-17 21:26:24 -0300

    Update versão interface.py

--- versão interface.py
+++ versão interface.py
@@ -25,3 +25,33 @@ while True:
     
 ano = int(values[0])
 mês = int(values[1])
+
+Último_dia_do_mês = 0
+    
+if mês == 1:
+    Último_dia_do_mês = 31
+elif mês == 2:
+    if ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0):
+        Último_dia_do_mês = 29
+    else:
+        Último_dia_do_mês = 28
+elif mês == 3:
+    Último_dia_do_mês = 31
+elif mês == 4:
+    Último_dia_do_mês = 30
+elif mês == 5:
+    Último_dia_do_mês = 31
+elif mês == 6:
+    Último_dia_do_mês = 30
+elif mês == 7:
+    Último_dia_do_mês = 31
+elif mês == 8:
+    Último_dia_do_mês = 31
+elif mês == 9:
+    Último_dia_do_mês = 30
+elif mês == 10:
+    Último_dia_do_mês = 31
+elif mês == 11:
+    Último_dia_do_mês = 30
+elif mês == 12:
+    Último_dia_do_mês = 31
