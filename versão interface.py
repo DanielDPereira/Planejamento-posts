4a5846a096eff5f1a62abbe2f14197b5636e202d
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-02-19 15:13:57 -0300

    Update versão interface.py

--- versão interface.py
+++ versão interface.py
@@ -15,6 +15,7 @@ layout = [  [sg.Text('Planejamento posts')],
             [sg.Text('Dias da semana onde serão postados vídeos: ')],
             [sg.InputText()],
             [sg.Text('Tipo do último post: '],
+            [sg.InputText()],
             [sg.Button('Ok'), sg.Button('Cancel')] ]
 
 # Create the Window
