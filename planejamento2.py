6765c857b8d9a91545ea7f8502cd4aea72ca9b09
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-04-23 20:17:21 -0300

    Update planejamento2.py

--- planejamento2.py
+++ planejamento2.py
@@ -5,6 +5,33 @@ import PySimpleGUI as sg
 ano = int(input("Ano: "))
 mês = int(input("Mês: "))
 
+'''
+sg.theme('DarkPurple1')   # Add a touch of color
+# All the stuff inside your wi
+# ndow.
+layout = [  [sg.Text('Planejamento posts')],
+            [sg.Text('Ano:')],
+            [sg.InputText()],
+            [sg.Text('Mês:')],
+            [sg.InputText()],
+            [sg.Text('Segunda = 0, Terça = 1, Quarta = 2, Quinta = 3, Sexta = 4, Sabádo = 5, Domingo = 6')],
+            [sg.Text('Insira os dias da semana de posts (separe com uma ","): ')],
+            [sg.InputText()],
+            [sg.Text('Dias da semana onde serão postados vídeos: ')],
+            [sg.InputText()],
+            [sg.Text('Explica = 0, Confirma = 1, Alerta = 2, ODS = 3, Indica = 4, Vídeo = 5')],
+            [sg.Text('Tipo do último post: ')],
+            [sg.InputText()],
+            [sg.Button('Ok'), sg.Button('Cancel')] ]    
+
+# Create the Window
+window = sg.Window('Relatório', layout)
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+'''
 if mês == 1 or mês == 3 or mês == 5 or mês == 7 or mês == 8 or mês == 10 or mês == 12:
     Último_dia_do_mês = 31
 elif mês == 2:
