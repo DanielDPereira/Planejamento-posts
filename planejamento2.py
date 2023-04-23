c13536d1cf95c43e43e4827bdc8239df3bb29660
Daniel Dias Pereira <dani.pereira8539@gmail.com>
2023-04-23 19:23:48 -0300

    Update planejamento2.py

--- planejamento2.py
+++ planejamento2.py
@@ -5,7 +5,7 @@ import PySimpleGUI as sg
 ano = int(input("Ano: "))
 mês = int(input("Mês: "))
 
-if mês == 1 or mês == 3 or mês == 5 or mês == 7:
+if mês == 1 or mês == 3 or mês == 5 or mês == 7 or mês == 8 or mês == 10 or mês == 12:
     Último_dia_do_mês = 31
 elif mês == 2:
     if ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0):
@@ -16,13 +16,7 @@ elif mês == 4:
     Último_dia_do_mês = 30
 elif mês == 6:
     Último_dia_do_mês = 30
-elif mês == 8:
-    Último_dia_do_mês = 31
 elif mês == 9:
     Último_dia_do_mês = 30
-elif mês == 10:
-    Último_dia_do_mês = 31
 elif mês == 11:
     Último_dia_do_mês = 30
-elif mês == 12:
-    Último_dia_do_mês = 31
